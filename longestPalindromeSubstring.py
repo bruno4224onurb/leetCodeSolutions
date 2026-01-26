0a9ac07bc2de50af70027c86de4b2d1bcaaeae1c
Bruno Franchi <144866243+bruno4224onurb@users.noreply.github.com>
2026-01-26 15:21:56 -0300

    Create longestPalindromeSubstring.py
    Given a string s, return the longest palindromic substring in s.
    
     
    
    Example 1:
    
    Input: s = "babad"
    Output: "bab"
    Explanation: "aba" is also a valid answer.
    Example 2:
    
    Input: s = "cbbd"
    Output: "bb"

---
+++ longestPalindromeSubstring.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        maiorPali = ""
+        for i in range(n):
+            l, r =i, i
+            while l >=0 and r < n and s[l] == s[r]:
+                if r - l + 1 > len(maiorPali):
+                    maiorPali = s[l:r+1]
+                l -= 1
+                r += 1
+            l, r = i, i+1
+            while l >=0 and r < n and s[l] == s[r]:
+                if r - l + 1 > len(maiorPali):
+                    maiorPali = s[l:r+1]
+                l -= 1
+                r += 1
+        
+        return maiorPali
+        
